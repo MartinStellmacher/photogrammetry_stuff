5009ca6562d9d5a74494f08a8a79e95d1e86afc6
Martin Stellmacher <martin@stellmachers.de>
2021-05-28 22:55:04 +0200

    nearly completed pandas transformation. Final cleanup required ...

--- charuco_calibration.py
+++ charuco_calibration.py
@@ -1,18 +1,19 @@
 import numpy as np
 import cv2
-import time
 from pathlib import Path
-import pickle
 import pandas as pd
 import multiprocessing as mp
-import sys
 
 import utilities
 
 
-def read_charucoboard(fname, img_scale, charuco_board_para, dict_type):
+def create_charucoboard(dict_type, charuco_board_para):
     dict = cv2.aruco.getPredefinedDictionary(dict_type)
     board = cv2.aruco.CharucoBoard_create(*charuco_board_para, dict)
+    return dict, board
+
+def detect_charucoboard(fname, img_scale, charuco_board_para, dict_type):
+    dict, board = create_charucoboard(dict_type, charuco_board_para)
     frame = utilities.read_scaled_image(fname, img_scale=img_scale)
     im_size = frame.shape[::-1]
     corners, ids, rejectedImgPoints = cv2.aruco.detectMarkers(frame, dict)
@@ -27,11 +28,11 @@ def read_charucoboard(fname, img_scale, charuco_board_para, dict_type):
         return fname, im_size, None
 
 
-def read_charucoboards(images, charuco_board_para, dict_type, image_scaling=1.0):
+def detect_charucoboards(images, charuco_board_para, dict_type, image_scaling=1.0):
     result = None
     im_size = None
     pool = mp.Pool(max(1, mp.cpu_count()-2))
-    results = [pool.apply_async(read_charucoboard, args=(fname, image_scaling, charuco_board_para, dict_type)) for fname in images]
+    results = [pool.apply_async(detect_charucoboard, args=(fname, image_scaling, charuco_board_para, dict_type)) for fname in images]
     for r in results:
         fname, one_size, res = r.get()
         if im_size is None:
@@ -45,54 +46,28 @@ def read_charucoboards(images, charuco_board_para, dict_type, image_scaling=1.0)
     return pd.Series(im_size, index = ['width', 'height']), result
 
 
-def read_charucoboards_seq(images, charuco_board_para, dict_type, image_scaling=1.0):
-    dict = cv2.aruco.getPredefinedDictionary(dict_type)
-    board = cv2.aruco.CharucoBoard_create(*charuco_board_para, dict)
-    result = None
-    im_size = None
-    for img in images:
-        print("=> Processing image {0}".format(img))
-        frame = utilities.read_scaled_image(img, img_scale=image_scaling)
-        # check size
-        if im_size is None:
-            im_size = frame.shape[::-1]
-        else:
-            assert im_size == frame.shape[::-1]
-        # first detect the markers
-        corners, ids, rejectedImgPoints = cv2.aruco.detectMarkers(frame, dict)
-        # then find the chessboard corners
-        if len(corners)>0:
-            n_final, pts_final, ids_final = cv2.aruco.interpolateCornersCharuco(corners,ids,frame,board)
-            if pts_final is not None and ids_final is not None and n_final>3:
-                img_pt_df = pd.DataFrame(pts_final.reshape((-1, 2)), columns=['img_pt_x', 'img_pt_y'],
-                             index=pd.MultiIndex.from_product([[img], ids_final.reshape(-1)],
-                                                              names=['image_file', 'point_id']))
-                result = pd.concat([result, img_pt_df])
-                if len( board.chessboardCorners) != n_final:
-                    print(f'\tdetected only {n_final} of {board.chessboardCorners} chessboard corners')
-            else:
-                print('\tfailed')
-    return pd.Series(im_size, index = ['width', 'height']), result
-
-
-# todo: second path without 10 worst images
-# todo: return Dataframe
-def calibrate_camera(allCorners, allIds, imsize, board):
+def calibrate_charuco(points, imsize, board):
     # fx == fy, absolute value has no meaning if CALIB_FIX_ASPECT_RATIO is specified
     # assume perfectly centered optics for the start
-    cameraMatrixInit = np.array([[ 1000.,    0., imsize[0]/2.],
-                                 [    0., 1000., imsize[1]/2.],
+    cameraMatrixInit = np.array([[ 1000.,    0., imsize.width/2.],
+                                 [    0., 1000., imsize.height/2.],
                                  [    0.,    0.,           1.]])
-
+    img_points = []
+    img_ids = []
+    image_names = points.index.get_level_values(0).unique()
+    for img in image_names:
+        img_pts = points.loc[img]
+        img_ids.append(img_pts.index.to_numpy().reshape((-1,1)))
+        img_points.append( img_pts[['img_pt_x','img_pt_y']].to_numpy().reshape((-1,1,2)))
     distCoeffsInit = np.zeros((5,1))
     # flags = (cv2.CALIB_USE_INTRINSIC_GUESS + cv2.CALIB_RATIONAL_MODEL + cv2.CALIB_FIX_ASPECT_RATIO)
     flags = (cv2.CALIB_FIX_ASPECT_RATIO)
-    (ret, camera_matrix, distortion_coefficients0,
+    (ret, camera_matrix, distortion_coefficients,
      rotation_vectors, translation_vectors,
      stdDeviationsIntrinsics, stdDeviationsExtrinsics,
      perViewErrors) = cv2.aruco.calibrateCameraCharucoExtended(
-                      charucoCorners=allCorners,
-                      charucoIds=allIds,
+                      charucoCorners=img_points,
+                      charucoIds=img_ids,
                       board=board,
                       imageSize=imsize,
                       cameraMatrix=cameraMatrixInit,
@@ -102,36 +77,44 @@ def calibrate_camera(allCorners, allIds, imsize, board):
     print(f'optics decentered {(np.divide(camera_matrix[0:2,2],np.array(imsize)/2)-1)*100} [%]')
     print(f'optics decentered {camera_matrix[0:2,2] - np.array(imsize)/2} [Pixel]')
 
-
-    return ret, camera_matrix, distortion_coefficients0, rotation_vectors, translation_vectors
-    # ret is ((perViewErrors*perViewErrors).sum()/len(perViewErrors))**0.5
-    # [1], [3x3], [5], [nImgx3x1], [nImgx3x1]
-    # stdDeviationsIntrinsics [2+2+5]
-    # stdDeviationsExtrinsics [nImgx2x3]
-    # perViewErrors [nImg]
-    # pandas result: [nImg x (rvec, tvec, std_dev_ext, per_view_errors)],
+    newcameramtx, roi = cv2.getOptimalNewCameraMatrix(camera_matrix, distortion_coefficients, tuple(imsize), 1,
+                                                      tuple(imsize))
+    stddev_extrinsics = stdDeviationsExtrinsics.reshape((-1, 2, 3))
+    extrinsics = None
+    for idx, _ in enumerate(image_names):
+        i_df = pd.concat([pd.DataFrame([translation_vectors[idx].reshape(-1)], columns=['tx', 'ty', 'tz']),
+                          pd.DataFrame([rotation_vectors[idx].reshape(-1)], columns=['rx', 'ry', 'rz']),
+                          pd.DataFrame([stddev_extrinsics[idx, 1]], columns=['stddev_tx', 'stddev_ty', 'stddev_tz']),
+                          pd.DataFrame([stddev_extrinsics[idx, 0]], columns=['stddev_rx', 'stddev_ry', 'stddev_rz']),
+                          pd.DataFrame([perViewErrors[idx]], columns=['rpe'])], axis=1)
+        extrinsics = pd.concat([extrinsics, i_df])
+    extrinsics = extrinsics.set_index(image_names)
+    intrinsics = pd.DataFrame([[camera_matrix[0,0], camera_matrix[1,1], camera_matrix[0,2], camera_matrix[1,2]]+distortion_coefficients.reshape((-1)).tolist(), stdDeviationsIntrinsics.reshape(-1)[:9]], columns=['fx', 'fy', 'cx', 'cy', 'k1', 'k2', 'p1', 'p2', 'k3'], index=['value', 'stddev'])
+    return intrinsics, extrinsics, pd.DataFrame(newcameramtx, columns=['fx', 'fy', 'c']), pd.Series(roi, index=['x', 'y', 'w', 'h'], name='roi')
 
 
 def perform_calibration( images, board_width, board_height, square_width, marker_width, dict_type, point_store, calibration_store, image_scaling=1.0):
     charuco_board_para = (board_width, board_height, square_width, marker_width)
     if point_store.exists():
-        with point_store.open('rb') as f:
-            allCorners, allIds, imsize = pickle.load( f)
+        size = pd.read_hdf(point_store, 'size')
+        points = pd.read_hdf(point_store, 'points')
     else:
-        t0 = time.time()
-        im_size, corners = read_charucoboards(images, charuco_board_para, dict_type, image_scaling)
-        print(f'{time.time()-t0:0.1f}s')
-        sys.exit()
-        # with point_store.open('wb') as f:
-        #     pickle.dump((allCorners, allIds, imsize), f)
-
-    # if os.path.exists( 'charuco_calibration.pkl'):
-    #     with open('charuco_calibration.pkl', 'rb') as f:
-    #         ret, mtx, dist, rvecs, tvecs = pickle.load( f)
-    # else:
-    #     ret, mtx, dist, rvecs, tvecs = calibrate_camera(allCorners, allIds, imsize, charuco_board)
-    #     with open('charuco_calibration.pkl', 'wb') as f:
-    #         pickle.dump( (ret, mtx, dist, rvecs, tvecs), f)
+        size, points = detect_charucoboards(images, charuco_board_para, dict_type, image_scaling)
+        size.to_hdf(point_store, 'size', mode='w')
+        points.to_hdf(point_store, 'points', mode='a')
+    if calibration_store.exists():
+        intrinsics = pd.read_hdf(calibration_store, 'intrinsics')
+        extrinsics = pd.read_hdf(calibration_store, 'extrinsics')
+        newcameramtx = pd.read_hdf(calibration_store, 'newcameramtx')
+        roi = pd.read_hdf(calibration_store, 'roi')
+    else:
+        _, board = create_charucoboard(dict_type, charuco_board_para)
+        intrinsics, extrinsics, newcameramtx, roi = calibrate_charuco(points, size, board)
+        intrinsics.to_hdf(calibration_store, 'intrinsics', mode='w')
+        extrinsics.to_hdf(calibration_store, 'extrinsics', mode='a')
+        newcameramtx.to_hdf(calibration_store, 'newcameramtx', mode='a')
+        roi.to_hdf(calibration_store, 'roi', mode='a')
+    return intrinsics, extrinsics, size, points, newcameramtx, roi
 
 
 def calibration_main(image_path, board_width, board_height, square_width, marker_width, dict_type, point_store='',
